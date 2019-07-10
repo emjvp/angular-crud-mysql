a041f43bad7b3a79dff7a071088ccd490434b7bc
emjvp <emjvp37@gmail.com>
2019-07-09 22:48:34 -0500

    cliente

--- server/src/controllers/gamesController.ts
+++ server/src/controllers/gamesController.ts
@@ -2,13 +2,33 @@ import { Request, Response } from 'express'
 import pool from '../database'
 class GamesController {
 
-    public index (req: Request, res: Response) {
-        pool.query('DESCRIBE games');
-        res.json("games")
+    public async list (req: Request, res: Response) {
+        const games = await pool.query('SELECT * FROM games');
+        res.json(games)
     } 
-    public create (req: Request, res: Response){
-        res.json({text: 'creating a game'})
+    public async getOne(req: Request, res: Response):Promise<any>{
+        const { id } = req.params
+        const game = await pool.query('SELECT * FROM games WHERE id = ?',id)
+        if(game.length > 0){
+            return res.json(game[0])
+        }        
+        res.status(404).json({text: "The game doesn't exits"})
     }
+    public async create (req: Request, res: Response): Promise<void>{
+        await pool.query('INSERT INTO games set ?', [req.body])
+        res.json({message: 'Game Saved'})
+    }
+    public async update(req: Request, res: Response): Promise<void>{
+        const { id } = req.params
+        await pool.query('UPDATE games set ? WHERE id = ?', [req.body, id]);
+        res.json({message: 'The game was updated'})
+    }
+    public async delete(req: Request, res: Response): Promise<void>{
+        const { id } = req.params
+        await pool.query('DELETE FROM games WHERE id = ?', [id])
+        res.json({message: 'The game was deleted'})
+    }
+    
 }
 
 const gamesController = new GamesController()
