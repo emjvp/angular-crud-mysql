71843fe78e4e06231eb24ba4b8a529d876644557
emjvp <emjvp37@gmail.com>
2019-08-11 22:16:05 -0500

    project advance

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { GameFormComponent } from './components/game-form/game-form.component';
 import { GameListComponent } from './components/game-list/game-list.component';
+import { GamesService } from './services/games.service';
 
 @NgModule({
   declarations: [
